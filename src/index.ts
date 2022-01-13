82f1bdb4a046f82c6d1ca15c3733286718b1d96c
Mohammad Korra <muhammadkorra@gmail.com>
2022-01-13 20:42:42 +0200

    MongoDb and Dotenv
    - Added MongoDb singleton class for usage across the app
    - Added dotenv .env file for passing variable db parameters

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import express, { Request, Response } from 'express'
+import MongoDB from './database/mongodb'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 const app = express()
+let mongoDb: MongoDB
 
 app.use(express.json())
 
@@ -10,6 +16,9 @@ app.get('/', (req: Request, res: Response): void => {
     })
 })
 
-app.listen(4040, (): void => {
+app.listen(4040, async (): Promise<void> => {
+    mongoDb = MongoDB.getInstance()
+    await mongoDb.connect()
+
     console.log('Started listening on 4040...')
 })
