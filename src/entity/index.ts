38ea09cdc4167793dc50c18fae92b0c45cfd219d
Mohammad Korra <muhammadkorra@gmail.com>
2022-01-15 00:01:05 +0200

    Added default value for SALT_ROUNDS if it is not defined in environment variables

--- src/entity/index.ts
+++ src/entity/index.ts
@@ -4,10 +4,7 @@ import EmailValidator from '../util/EmailValidator'
 import PasswordManager from '../util/PasswordManager'
 import UserFactory from './User'
 
-const userFactory = new UserFactory(
-    new UUIDv4(),
-    new EmailValidator(),
-    new PasswordManager(parseInt(process.env.SALT_ROUNDS, 10))
-)
+const saltRounds: number = process.env.SALT_ROUNDS ? parseInt(process.env.SALT_ROUNDS, 10) : 10
+const userFactory = new UserFactory(new UUIDv4(), new EmailValidator(), new PasswordManager(saltRounds))
 
 export { userFactory }
